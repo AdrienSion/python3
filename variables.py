0af5b85931056cdf75dbac29aea89f37d83c175e
Adrien <adriension22@gmail.com>
2022-02-28 16:06:38 +0100

    Création du repo
    Ajout des premiers chapitre de cours.
    Ajout des exo correspondants.

---
+++ variables.py
@@ -0,0 +1,41 @@
+from operator import truediv
+# Commentait ctrl shift :
+# shift alt T = ouvrir terminal
+
+nombre = 123
+print(nombre)
+
+# On peut écrire un entier avec .0 pour dire que c'est un float
+nombre = 3.14
+print(nombre)
+
+nombre = "123"
+print(nombre)
+
+text = "lorem ipsum"
+print(text)
+
+is_day = True
+print(is_day)
+
+has_sugar = False
+print(has_sugar)
+
+has_accepted_ula = None
+print(has_accepted_ula)
+
+html_code = '<h1>Titre de mon blog</h1>'
+print(html_code)
+
+nickname = "John \"doeuf\" doe"
+nickname = "John O\"Connor"
+print(nickname)
+
+multiline_text = "foo\nbar\nbaz"
+print(multiline_text)
+
+multiline_text = """foo
+bar
+baz"""
+print(multiline_text)
+
