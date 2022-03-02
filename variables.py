1ad37429a6a57c8cda72167a1be2152fa4dc98a6
Adrien <adriension22@gmail.com>
2022-03-02 11:58:19 +0100

    MIse à jour cours variables
    Exploration de conversions de type

--- variables.py
+++ variables.py
@@ -4,19 +4,26 @@ from operator import truediv
 
 nombre = 123
 print(nombre)
+# type permet de savoir de quel type de données il s'agit
+print(type(nombre))
+
 
 # On peut écrire un entier avec .0 pour dire que c'est un float
 nombre = 3.14
 print(nombre)
+print(type(nombre))
 
 nombre = "123"
 print(nombre)
+print(type(nombre))
+# exit() permet de mettre fin à la requette
 
 text = "lorem ipsum"
 print(text)
 
 is_day = True
 print(is_day)
+print(type(is_day))
 
 has_sugar = False
 print(has_sugar)
@@ -39,3 +46,74 @@ bar
 baz"""
 print(multiline_text)
 
+# Fonctions = code qui effectue des opérations 
+
+# type()
+
+nombre = "2.71"
+print(nombre)
+print(type(nombre))
+
+nombre = float(nombre)
+print(nombre)
+print(type(nombre))
+
+nombre = 3.14
+print(nombre)
+print(type(nombre))
+
+nombre = int(nombre)
+print(nombre)
+print(type(nombre))
+
+texte = str(nombre)
+print(texte)
+print(type(texte))
+
+my_var = 0
+my_var = bool(my_var)
+print(my_var)
+
+my_var = 1
+my_var = bool(my_var)
+print(my_var)
+
+my_var = -123
+my_var = bool(my_var)
+print(my_var)
+
+my_var = "0"
+my_var = bool(my_var)
+print(my_var)
+
+my_var = ""
+my_var = bool(my_var)
+print(my_var)
+
+my_var = " "
+my_var = bool(my_var)
+print(my_var)
+
+my_var = "123"
+my_var = bool(my_var)
+print(my_var)
+
+my_var = "-123"
+my_var = bool(my_var)
+print(my_var)
+
+my_var = [123, "abc", False]
+my_var = bool(my_var)
+print(my_var)
+
+my_var = [False]
+my_var = bool(my_var)
+print(my_var)
+
+my_var = [None]
+my_var = bool(my_var)
+print(my_var)
+
+my_var = None
+my_var = bool(my_var)
+print(my_var)
